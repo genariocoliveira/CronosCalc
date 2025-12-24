97575f685cf573e9851128110f7c93a331ce1cd0
Genário Carneiro <53302984+genariocoliveira@users.noreply.github.com>
2025-12-24 10:16:01 -0300

    feat: Update dependencies and build configuration
    Updates React and ReactDOM to the latest versions, enabling proper ESM imports.
    Disables sourcemap generation for production builds and configures Vite for a specific port.
    Introduces Vercel configuration for seamless deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  },
+  server: {
+    port: 3000
   }
 });
